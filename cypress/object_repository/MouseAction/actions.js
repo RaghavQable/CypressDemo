7e424dfef46e8a05c81fff7d902648b49ffdbb93
mahesh <mahesh@qable.io>
2023-05-15 09:46:28 +0530

    added comments

--- cypress/object_repository/MouseAction/actions.js
+++ cypress/object_repository/MouseAction/actions.js
@@ -1,42 +1,52 @@
 // actions on element
 
-export function clickOnElement(locator) {
-    cy.get(locator).click();
+export function clickOnElement(locator, ...n) {
+    // click on the element 
+    cy.get(locator).eq(n).scrollIntoView().should('be.visible').click();
 }
 
 export function doubleClickOnElement(locator) {
-    cy.get(locator).dblclick();
+    // action of the double click single element  
+    cy.get(locator).first().dblclick();
 }
 
 export function selectValue(locator, value) {
+    //  verify the element css value
     cy.get(locator).select(value);
 }
 
 export function checkElement(locator) {
+    //  verify the this element is checked 
     cy.get(locator).check().should('be.checked');
 }
 
 export function checkValue(locator, value) {
+    //verify the check same as element value 
     cy.get(locator).check(value);
 }
 
 export function uncheckElement(locator) {
+    //  verify the element is unchecked 
     cy.get(locator).uncheck().should('not.be.checked');
 }
 
 export function uncheckValue(locator, value) {
+    // verify the element the value is unchecked
     cy.get(locator).uncheck(value);
 }
 
 export function clearText(locator) {
+    // /This function working as for the  input text box clear 
     cy.get(locator).clear();
 }
 
 export function type(locator, value) {
+     // /This function working as for the  input text box clear and enter the string text 
     cy.get(locator).clear().type(value);
 }
 
 export function scrollToElement(locator) {
+    //  this function use as the scrollIntoView if needed
     cy.get(locator).scrollIntoView();
 }
 
@@ -46,10 +56,12 @@ export function scrollToPage(locator, position) {
 }
 
 export function submit(locator) {
+    //  This function directly use for the submission.
     cy.get(locator).submit();
 }
 
 export function pageReload() {
+    //  This function uses for the browser page reload action
     cy.reload();
 }
 
